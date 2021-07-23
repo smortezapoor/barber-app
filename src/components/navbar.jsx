225adb455c28d1e1c602033d32feffa6e28f32cf
Farnaz Dini <farnaz.dini@gmail.com>
2021-07-23 20:29:12 +0200

    navbar fixed

---
+++ src/components/navbar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top navbar-bg-blue">
+      <div className="container-fluid">
+        <a className="navbar-brand  dropdown" href="#">
+          Barber App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>{" "}
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Barbers
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Servics
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Finding Barbers
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Finding Customers
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Your Opinion
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link disabled"
+                href="#"
+                tabIndex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn  navbar-bg-blue-search-btn" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
